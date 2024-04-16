e38c249e2cf0e03d6d3375ecb14745f269d83223
Radu Nicolae <rnicolae04@gmail.com>
2024-04-16 12:29:45 +0200

    code runnable

--- opendc-trace/opendc-trace-api/build.gradle.kts
+++ opendc-trace/opendc-trace-api/build.gradle.kts
@@ -26,3 +26,10 @@ description = "Workload trace library for OpenDC"
 plugins {
     `kotlin-library-conventions`
 }
+dependencies {
+
+    implementation(libs.jackson.dataformat.csv)
+
+    implementation(project(mapOf("path" to ":opendc-trace:opendc-trace-parquet")))
+    testImplementation(project(mapOf("path" to ":opendc-trace:opendc-trace-testkit")))
+}
