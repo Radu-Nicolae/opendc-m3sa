47ca53424fe1399c30bef0ab37a9dcf1d013fc91
Radu Nicolae <rnicolae04@gmail.com>
2024-12-07 20:14:28 +0100

    exp 3 data -- metamodels finalized with the right timestamps and carbon intensity

--- exp3/co2_migrator.py
+++ exp3/co2_migrator.py
@@ -3,24 +3,20 @@ import pandas as pd
 import os
 from migrator import *
 from plotter import *
-
-SCALE_TO_MARCONI = 2982 / 150
+from variables import *
 
 def co2_traces(path):
     metamodels = []
     for file in os.listdir(path):
-        # if the file ends in parquet and doesn't have EU in the name
         if file.endswith('.parquet') and "EU" not in file:
             metamodels.append(
                 MetaModel(
                     country_code = file[:2],
-                    co2_emissions = pd.read_parquet(path + file)["carbon_intensity"].values
+                    co2_emissions = pd.read_parquet(path + file)["carbon_emission"].values
                 )
             )
 
     for metamodel in metamodels:
-        # add the timestamps such that each metamodel has as many timestamps as values in carbon intenstity.
-        # the timestamps start from 1 and go as  1, 2, 3, 4 ...
         metamodel.timestamps = np.arange(1, len(metamodel.co2_emissions) + 1)
 
 
@@ -77,37 +73,15 @@ def save_model_to_file(metamodels, migrated, path):
     min_entries = min(len(timestamps), len(migrated.co2_emissions))
     df = pd.DataFrame({
         "timestamp": timestamps[:min_entries],
-        "carbon_intensity": migrated.co2_emissions[:min_entries]
+        "carbon_emission": migrated.co2_emissions[:min_entries]
     })
 
     df.to_parquet(path)
 
 
 if __name__ == '__main__':
-    metamodels = co2_traces("inputs/co2/")
+    metamodels = co2_traces("metamodels/")
     metamodels = align_metamodels_by_size(metamodels)
-    migration_15min = migrate(metamodels, granularity=1, country_code="EU-15min")[1]
-    save_model_to_file(metamodels, migration_15min, path="inputs/EU-migration=15min-2023-06.parquet")
-
-    migration_1h = migrate(metamodels, granularity=4, country_code="EU-1h")[1]
-    migration_4h = migrate(metamodels, granularity=16, country_code="EU-4h")[1]
-    migration_8h = migrate(metamodels, granularity=32, country_code="EU-8h")[1]
-    migration_1d = migrate(metamodels, granularity=96, country_code="EU-1d")[1]
-    save_model_to_file(
-        metamodels,
-        ,
-        path="inputs/EU-migration=1d-2023-06.parquet"
-    )
-
-    metamodels.append(migration_15min)
-    metamodels.append(migration_1h)
-    metamodels.append(migration_4h)
-    metamodels.append(migration_8h)
-    metamodels.append(migration_1d)
-
     for metamodel in metamodels:
-
-
-    plotter(metamodels)
-    # plotter(metamodels)
+        print(metamodel.country_code, metamodel.total_emissions)
 
--- exp3/migrator.py
+++ exp3/migrator.py
@@ -4,124 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 
-from exp3.co2_migrator import SCALE_TO_MARCONI
-
-# recorded data
-#       | MIGRATION COUNT PER INTERVAL|
-# Month  15min  60min  240min  1440min
-# Jan    4      4        4      2
-# Feb    24     24       20     2
-# Mar    73     73       33     3
-# Apr    43     43       25     7
-# May    17     17       6      0
-# Jun    112    112      69     11
-# Jul    105    105      46     7
-# Aug    112    112      58     8
-# Sep    30     30       21     0
-# Oct    112    111      53     8
-# Nov    62     62       28     6
-# Dec    53     53       16     3
-
-# Granularity 15 leads to total emissions of 59.43 kg with 112 migrations
-# Granularity 60 leads to total emissions of 59.43 kg with 112 migrations
-# Granularity 240 leads to total emissions of 137.3 kg with 69 migrations
-# Granularity 1440 leads to total emissions of 124.22 kg with 11 migrations
-
-# experiment results
-# FR ----- 189.01
-# CH ----- 67.35
-# RO ----- 6654.41
-# ES ----- 3075.23
-# PT ----- 679.89
-# AT ----- 537.28
-# DE ----- 11419.32
-# IT ----- 1274.66
-# DK ----- 1407.52
-# BA ----- 6026.98
-# LU ----- 3365.57
-# MK ----- 7242.4
-# SK ----- 615.13
-# HU ----- 5404.42
-# BE ----- 608.74py
-# HR ----- 1316.96
-# SE ----- 81.73
-# LV ----- 453.57
-# GR ----- 1634.75
-# CZ ----- 2980.95
-# NO ----- 85.8
-# SI ----- 2474.47
-# FI ----- 617.16
-# BG ----- 2049.83
-# NL ----- 3504.91
-# PL ----- 6124.35
-# LT ----- 851.8
-# RS ----- 5144.83
-# EE ----- 2666.25
-# Granularity 15 leads to total emissions of 59.43 kg with 112 migrations
-# Granularity 60 leads to total emissions of 59.43 kg with 112 migrations
-# Granularity 240 leads to total emissions of 137.3 kg with 69 migrations
-# Granularity 1440 leads to total emissions of 124.22 kg with 11 migrations
-
-
-colorblind_friendly_colors = [
-    "#E69F00",  # orange
-    "#56B4E9",  # sky blue
-    "#009E73",  # bluish green
-    "#F0E442",  # yellow
-    "#0072B2",  # blue
-    "#D55E00",  # vermilion
-    "#CC79A7",  # reddish purple
-
-    "#F0A3FF",  # bright purple
-    "#0075DC",  # cerulean
-    "#993F00",  # burnt orange
-    "#4C005C",  # dark purple
-    "#191919",  # grey
-    "#005C31",  # dark green
-    "#2BCE48",  # green
-    "#FFCC99",  # peach
-    "#808080",  # gray
-    "#94FFB5",  # mint
-    "#8F7C00",  # mustard
-    "#9DCC00",  # lime
-    "#C20088",  # magenta
-
-    "#003380",  # navy blue
-    "#FFA405",  # tangerine
-    "#FFA8BB",  # pink
-    "#426600",  # olive
-    "#FF0010",  # red orange
-    "#5EF1F2",  # bright cyan
-    "#00998F",  # teal
-    "#E0FF66",  # light lime
-    "#740AFF",  # violet
-    "#990000",  # dark red
-
-    "#FFFF80",  # light yellow
-    "#FF5005",  # bright orange
-    "#FFFF00",  # yellow
-    "#FFFFFF",  # white
-    "#000000",  # black
-    "#FFB200",  # vivid orange
-    "#ABCDEF",  # pale blue
-    "#FA58F4",  # hot pink
-    "#00FFCC",  # turquoise
-    "#D7DF01",  # yellowish green
-
-    "#8A4117",  # sienna
-    "#5CB3FF",  # sky blue
-    "#B0E57C",  # tea green
-    "#F5E216",  # golden yellow
-    "#7851A9",  # royal purple
-    "#4A9586",  # blue green
-    "#FF0028",  # scarlet
-    "#E55B3C",  # terra cotta
-    "#C6DC67",  # pistachio
-]
-SCALE_TO_MARCONI = 2982 / 150
-
-linestyles = ['-', '--', '-.', ':']
-markers = ['o', 'v', '^', '<', '>', 's', 'p', '*', 'h', 'H', 'D', 'd', 'P', 'X']
+from variables import *
 
 
 class MetaModel:
@@ -131,7 +14,6 @@ class MetaModel:
             self.timestamps = timestamps.tolist()
         except:
             self.timestamps = timestamps
-        self.timestamps_minutes = []
 
         try:
             self.co2_emissions = co2_emissions.tolist()
@@ -142,9 +24,8 @@ class MetaModel:
             self.trim_metamodels_by_time(start, end)
 
         self.total_emissions = sum(self.co2_emissions)
-        self.total_emissions = self.total_emissions# emissions in tons   , at marconi scale
+        self.total_emissions = self.total_emissions  # emissions in tons   , at marconi scale
         self.total_emissions = round(self.total_emissions, 2)
-
 
     def trim_metamodels_by_time(self, start, end):
         """
@@ -169,10 +50,7 @@ class MetaModel:
                 break
 
         self.timestamps = self.timestamps[index_of_start:index_of_termination]
-        self.timestamps_minutes = self.timestamps_minutes[index_of_start:index_of_termination]
         self.co2_emissions = self.co2_emissions[index_of_start:index_of_termination]
-
-
 
 
 def get_metamodels(path):
@@ -219,53 +97,7 @@ def minimum_at_timestamp(metamodels, timestamp):
     return min, location
 
 
-def plot_total_emissions(metamodels):
-    # plot a data distribution, per country, as a boxplot
-    data = []
-    for metamodel in metamodels:
-        data.append(metamodel.total_emissions)
 
-    plt.figure(figsize=(5, 5))
-    plt.boxplot(data, vert=False)
-    plt.xlabel("Total CO2 emissions (tons)")
-    plt.show()
-
-
-def plot_all_emissions(metamodels, start, end, plot_title):
-    plt.figure(figsize=(20, 10))
-
-    i = 0
-    for metamodel in metamodels:
-        timestamps = pd.to_datetime(metamodel.timestamps)
-        plt.plot(timestamps, metamodel.co2_emissions, label=metamodel.country_code, color=colorblind_friendly_colors[i],
-                 width=5, linestyle=linestyles[i % len(linestyles)])
-        i += 1
-
-    padding = pd.Timedelta(days=1)  # Adjust as needed
-    plt.xlim(start - padding, end + padding)
-
-    # Define x-ticks
-    num_ticks = 4  # Adjust as needed
-    x_ticks_positions = pd.date_range(start, end, periods=num_ticks)
-    x_ticks_labels = [date.strftime('%d/%m') for date in x_ticks_positions]  # Labels without the year
-    plt.xticks(ticks=x_ticks_positions, labels=x_ticks_labels, fontsize=38)  # Bigger font size
-
-    # Set y-axis range
-    min_co2, max_co2 = -30, 500  # Specify your minimum and maximum y-values here
-    plt.ylim(min_co2, max_co2)
-
-    y_ticks = [0, 100, 200, 300, 400]
-    plt.yticks(fontsize=38, ticks=y_ticks)  # Bigger font size
-    plt.ylabel("CO2 Emission (gCO2/h)", fontsize=38)
-    plt.xlabel("Time (day/month)", fontsize=38)
-    plt.grid(True)
-
-    # Adjust plot margins  # Adjust the left and right padding here
-    # the legend will be on two rows, and many columns
-    # thickness of the lines is 5
-    plt.legend(loc='upper center', fancybox=True, shadow=True, ncol=10, fontsize=58, bbox_to_anchor=(0.5, 1.15))
-    plt.savefig(plot_title)
-    plt.show()
 
 
 def get_lowest_emission_location_at_timestamp(metamodels, timestamp_index):
@@ -311,11 +143,7 @@ def migrate_at_granularity(metamodels, granularity):
 
         i += 1
 
-    metamodel = MetaModel(
-        timestamps=metamodels[0].timestamps,
-        co2_emissions=co2_emissions,
-        country_code="EU"
-    )
+    metamodel = MetaModel(timestamps=metamodels[0].timestamps, co2_emissions=co2_emissions, country_code="EU")
 
     return migration_count, metamodel
 
@@ -330,7 +158,6 @@ def align_metamodels_by_size(metamodels):
     for metamodel in metamodels:
         metamodel.timestamps = metamodel.timestamps[:shortest]
         metamodel.co2_emissions = metamodel.co2_emissions[:shortest]
-        metamodel.timestamps_minutes = metamodel.timestamps_minutes[:shortest]
 
     return metamodels
 
@@ -340,11 +167,18 @@ def output_analysis():
         # print all the relevant details on one line with new line at the end
         print(f"{metamodel.country_code} ----- {metamodel.total_emissions}")
 
-
-    print("Granularity 15 leads to total emissions of ", migrate_at_granularity(metamodels, granularity=15)[1].total_emissions, "kg with", migrate_at_granularity(metamodels, granularity=15)[0], "migrations")
-    print("Granularity 60 leads to total emissions of ", migrate_at_granularity(metamodels, granularity=60)[1].total_emissions, "kg with", migrate_at_granularity(metamodels, granularity=60)[0], "migrations")
-    print("Granularity 240 leads to total emissions of ", migrate_at_granularity(metamodels, granularity=240)[1].total_emissions, "kg with", migrate_at_granularity(metamodels, granularity=240)[0], "migrations")
-    print("Granularity 1440 leads to total emissions of ", migrate_at_granularity(metamodels, granularity=1440)[1].total_emissions, "kg with", migrate_at_granularity(metamodels, granularity=1440)[0], "migrations")
+    print("Granularity 15 leads to total emissions of ",
+          migrate_at_granularity(metamodels, granularity=15)[1].total_emissions, "kg with",
+          migrate_at_granularity(metamodels, granularity=15)[0], "migrations")
+    print("Granularity 60 leads to total emissions of ",
+          migrate_at_granularity(metamodels, granularity=60)[1].total_emissions, "kg with",
+          migrate_at_granularity(metamodels, granularity=60)[0], "migrations")
+    print("Granularity 240 leads to total emissions of ",
+          migrate_at_granularity(metamodels, granularity=240)[1].total_emissions, "kg with",
+          migrate_at_granularity(metamodels, granularity=240)[0], "migrations")
+    print("Granularity 1440 leads to total emissions of ",
+          migrate_at_granularity(metamodels, granularity=1440)[1].total_emissions, "kg with",
+          migrate_at_granularity(metamodels, granularity=1440)[0], "migrations")
 
     plt.figure(figsize=(500, 10))
     for metamodel in metamodels:
